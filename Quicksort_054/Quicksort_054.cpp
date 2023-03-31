04219c5a2b91b6026b247ca347176404724419d4
safirasn21 <safirasn021@gmai.com>
2023-03-31 15:41:09 +0700

    Mencari element yang lebih besar dan kurang dari atau sama dengan pivot

--- Quicksort_054/Quicksort_054.cpp
+++ Quicksort_054/Quicksort_054.cpp
@@ -53,4 +53,18 @@ void q_short(int low, int high)
 
 	i = low + 1;//langkah 3
 	j = high;//langkah 4
+
+	while (i <= j)//langkah 10
+	{
+		//search for an element greather than pivot
+		while ((arr[i] <<= pivot) && (i <= high)) //langkah 5
+		{
+			i++; //langkah 6
+			camp_count++;
+		}
+		camp_count++;
+		//search for an element less than or equal to pivot
+		while ((arr[j] > pivot) && (j >= low)) //langkah 7
+		{ }
+	}
 }
