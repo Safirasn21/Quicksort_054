0ce28276d33f127154b7a8324db0671a3728fe2c
safirasn21 <safirasn021@gmai.com>
2023-03-31 15:01:44 +0700

    procedure untuk menyimpan nilai integer

--- Quicksort_054/Quicksort_054.cpp
+++ Quicksort_054/Quicksort_054.cpp
@@ -1,1 +1,8 @@
+#include <iostream>
+using namespace std;
 
+//Array of integers to hold value
+int arr[20];
+int camp_count = 0; // number of comparison
+int mov_count = 0; // number of data movement
+int n;
